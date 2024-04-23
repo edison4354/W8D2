8eb55519e361c3d9fbc8e7a97fa0a1a00af4947a
Edison <edison4354@gmail.com>
2024-04-23 13:39:36 -0700

    Completed Phase 3

--- practice-for-ch-js-intro-js-exercises-long-practice/phase-3-iteration.js
+++ practice-for-ch-js-intro-js-exercises-long-practice/phase-3-iteration.js
@@ -1,1 +1,36 @@
 // Your code here
+Array.prototype.bubbleSort = function() {
+    let sorted = false;
+
+    while(!sorted) {
+        sorted = true
+        for (let i = 0; i < this.length - 1; i++ ) {
+            let j = i+1 
+            if (this[i] > this[j]) {
+                let temp = this[i]
+                this[i] = this[j]
+                this[j] = temp
+                sorted = false
+            }
+        };
+    }
+    return this
+};
+
+
+arr = [6, 5, 3, 1, 8]
+
+console.log(arr.bubbleSort());
+
+String.prototype.substrings = function() {
+    let arr = []
+    for (let i = 0; i < this.length; i++) {
+        for (let j = i+1; j <= this.length; j++) {
+            arr.push(this.slice(i, j));
+        };
+    };
+    return arr
+};
+
+const string = "Hello" // -> ['H', 'He', 'Hel', etc...]
+console.log(string.substrings())
